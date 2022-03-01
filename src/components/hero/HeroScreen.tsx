f6d778bee55850a2842149fa9747224053c8adad
José Carlos Palma <palmahn@gmail.com>
2022-02-28 21:01:06 -0600

    feat(app): Creación del HeroScreen.

---
+++ src/components/hero/HeroScreen.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material';
+
+export const HeroScreen = () => {
+    return (
+        <>
+            <Typography variant="h4">Hero Screen</Typography>
+        </>
+    );
+};
--- src/routers/HomeRoutes.tsx
+++ src/routers/HomeRoutes.tsx
@@ -5,6 +5,7 @@ import { Navbar } from '../components/ui/Navbar';
 import { MarvelScreen } from '../components/marvel/MarvelScreen';
 import { DcScreen } from '../components/dc/DcScreen';
 import { SearchScreen } from '../components/search/SearchScreen';
+import { HeroScreen } from '../components/hero/HeroScreen';
 
 export const HomeRoutes = () => {
     return (
@@ -28,6 +29,8 @@ export const HomeRoutes = () => {
                         <Route path="marvel" element={<MarvelScreen />} />
                         <Route path="dc" element={<DcScreen />} />
                         <Route path="search" element={<SearchScreen />} />
+                        <Route path="hero" element={<HeroScreen />} />
+
                         <Route path="/" element={<MarvelScreen />} />
                     </Routes>
                 </Box>
