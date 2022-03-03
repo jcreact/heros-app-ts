63c9a69b0bbb3aaf9727ac6b0657f422aa2354be
José Carlos Palma <palmahn@gmail.com>
2022-03-02 21:23:52 -0600

    feat(hooks): Creación del Hook para manejar los valores del formulario.

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,35 @@
+import { ChangeEvent, useState } from 'react';
+
+const convertTo = <T, V>(s: T, v: V) =>
+    Object.keys(s).reduce((o, k) => ({ ...o, [k]: v }), {} as Record<keyof T, V>);
+
+export const useForm = <T>(state: T) => {
+    const [formValues, setFormValues] = useState(state);
+    const [touched, setTouched] = useState(convertTo<T, boolean>(state, false));
+
+    const formReset = () => {
+        setFormValues(state);
+        setTouched(convertTo<T, boolean>(state, false));
+    };
+
+    const handleChange = ({ target }: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        setFormValues({
+            ...formValues,
+            [target.name]: target.value,
+        });
+
+        setTouched({
+            ...touched,
+            [target.name]: true,
+        });
+    };
+
+    const isTouched = (name: keyof T) => touched[name];
+
+    return {
+        formValues,
+        handleChange,
+        formReset,
+        isTouched,
+    };
+};
